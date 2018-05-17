1efe72af27f3a3c7f1fb23f7b79e2536a5ffc91e
vphatarp <vphatarp@g>
2018-05-17 08:56:17 -0700

    Adding comments for better readability

--- stringParser.cpp
+++ stringParser.cpp
@@ -15,14 +15,24 @@ public:
 	void parseString(std::vector<std::string>& vec, std::string line);
 };
 
+//The entire logic for parsing is in the method below
 void stringParser::parseString(std::vector<std::string> &vec, std::string line){
 	size_t pos;
+        //Stores the position of the delimiter
 	int i=0;
+        //npos is the last possible position
+        //Here the position of the delimiter is checked
 	while((pos = line.find(delimiter)) != std::string::npos){
+               //the statement below parses the string till pos but excluding it
+               //So what we get in vec is the first string
 		vec[i] = line.substr(0, pos);
+               //The above command does not modify the string
+               //The line below takes out the already processes part of the string
 		line.erase(0, pos+delimiter.length());
 		i++;
 	}
+        //Since for the last string the delimiter would not exist
+        //It would be saved outside the loop
 	vec[i] = line;
 }
 int main(){
